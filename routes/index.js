e9a420273f8192a6fe8a2b7955e482ebf1a7c3f6
DonnyAndre29 <donandre43@yahoo.com>
2024-03-18 17:48:25 -0700

    route

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const homeRoute = require('./homeRoute');
 // const apiRoutes = require('./api');
 // const googleRoute = require('./googleRoutes')
-const dashboard = require('./dashboard')
+// const dashboard = require('./dashboard')
 
 
 router.use('/', homeRoute);
-router.use('/success', dashboard);
+// router.use('/success', dashboard);
 // router.use('/signup', googleRoute)
 
 // router.use('/api', apiRoutes);
